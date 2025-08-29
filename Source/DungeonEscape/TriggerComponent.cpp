30b25fe87ce271893b0e15f411dac120f87d4c6d
Programmer25X <denniscullipcross@icloud.com>
2025-08-29 18:43:45 +0100

    Added TriggerComponent C++ code

---
+++ Source/DungeonEscape/TriggerComponent.cpp
@@ -0,0 +1,14 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "TriggerComponent.h"
+
+void UTriggerComponent::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void UTriggerComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                           
+}
